35f297c8f74fedd3f57f3c1b689c3813a4b70aff
AllenSun7 <sunlun7@yahoo.com>
2020-03-07 16:41:51 -0800

    evaluation function

--- project2-multiagent/multiAgents.py
+++ project2-multiagent/multiAgents.py
@@ -18,6 +18,7 @@ import random, util
 
 from game import Agent
 import pysnooper
+import numpy as np
 
 class ReflexAgent(Agent):
     """
@@ -71,11 +72,51 @@ class ReflexAgent(Agent):
         successorGameState = currentGameState.generatePacmanSuccessor(action)
         newPos = successorGameState.getPacmanPosition()
         newFood = successorGameState.getFood()
+        new_food_list = newFood.asList()
         newGhostStates = successorGameState.getGhostStates()
+        new_ghost_pos = successorGameState.getGhostPositions()
+        num_ghosts = successorGameState.getNumAgents()-1
         newScaredTimes = [ghostState.scaredTimer for ghostState in newGhostStates]
-
         "*** YOUR CODE HERE ***"
-        return successorGameState.getScore()
+        
+        #heuristic distance to food 
+        dis_food = [((newPos[0]-f[0])**2 + (newPos[1]-f[1])**2)**0.5 for f in new_food_list]
+        #if food is not empty
+        if dis_food:
+            food_nearest_idx = dis_food.index(min(dis_food))
+            pos_food_nearest = new_food_list[food_nearest_idx]
+            dis_food_nearest = dis_food[food_nearest_idx]
+        else: 
+            dis_food_nearest = 1
+        #heuristic distance to ghost
+        dis_ghost = [((newPos[0]-g[0])**2 + (newPos[1]-g[1])**2)**0.5 for g in new_ghost_pos]
+        ghost_nearest_idx = dis_ghost.index(min(dis_ghost))
+        pos_ghost_nearest = new_ghost_pos[ghost_nearest_idx]
+        dis_ghost_nearest = dis_ghost[ghost_nearest_idx]
+
+        # print("newFood: ")
+        # print(str(newFood))
+        # print(new_food_list)
+        # print("newPos: ")
+        # print(str(newPos))
+        # print("num_ghosts: ")
+        # print(str(num_ghosts))       
+        # print("new_ghost_pos: ")
+        # print(str(new_ghost_pos))  
+        # print("successorGameState.getScore():", successorGameState.getScore())     
+        # print("dis_food_nearest:", dis_food_nearest)     
+        # print("dis_ghost_nearest:", dis_ghost_nearest) 
+
+        # distance to nearest food: ther closer, the better
+        score_dis_food = 1 / dis_food_nearest if dis_food_nearest != 0 else 0
+        # distance to ghost: ther further, the better
+        score_dis_ghost = - 1 / dis_ghost_nearest if dis_ghost_nearest != 0 else 0
+        # pacman successor score 
+        score = successorGameState.getScore() + score_dis_food + score_dis_ghost
+        return score
+          
+        
+
 
 def scoreEvaluationFunction(currentGameState):
     """
@@ -140,16 +181,16 @@ class MinimaxAgent(MultiAgentSearchAgent):
         def evalute_score(state, depth):
             return state.isWin() or state.isLose() or depth == self.depth
 
-        def min_value(state, depth, ghost_index):
+        def min_value(state, depth, g_idx):
             if evalute_score(state, depth):
                 return self.evaluationFunction(state)
 
             v = 99999999999999
-            for action in state.getLegalActions(ghost_index):
-                if ghost_index == num_ghosts:
-                    v = min(v, max_value(state.generateSuccessor(ghost_index, action), depth+1))
+            for action in state.getLegalActions(g_idx):
+                if g_idx == num_ghosts:
+                    v = min(v, max_value(state.generateSuccessor(g_idx, action), depth+1))
                 else:
-                    v = min(v, min_value(state.generateSuccessor(ghost_index, action), depth, ghost_index+1))
+                    v = min(v, min_value(state.generateSuccessor(g_idx, action), depth, g_idx+1))
             #print("min value: ", v)
             return v
         
@@ -181,17 +222,17 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        def min_value(state, depth, ghost_index, alpha, beta):
+        def min_value(state, depth, g_idx, alpha, beta):
             # if trigger score evaluate funtion
             if state.isWin() or state.isLose() or depth == self.depth:
                 return self.evaluationFunction(state)
 
             v = 99999999999999
-            for action in state.getLegalActions(ghost_index):
-                if ghost_index == num_ghosts:
-                    v = min(v, max_value(state.generateSuccessor(ghost_index, action), depth+1, alpha, beta)[0])
+            for action in state.getLegalActions(g_idx):
+                if g_idx == num_ghosts:
+                    v = min(v, max_value(state.generateSuccessor(g_idx, action), depth+1, alpha, beta)[0])
                 else:
-                    v = min(v, min_value(state.generateSuccessor(ghost_index, action), depth, ghost_index+1, alpha, beta))
+                    v = min(v, min_value(state.generateSuccessor(g_idx, action), depth, g_idx+1, alpha, beta))
                 #prune 
                 if v < alpha:
                     return v
@@ -240,7 +281,48 @@ class ExpectimaxAgent(MultiAgentSearchAgent):
         legal moves.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+
+        def chance(state, depth, g_idx):
+            # if trigger score evaluate funtion
+            if state.isWin() or state.isLose() or depth == self.depth:
+                return self.evaluationFunction(state)
+            
+            #initial value, probability 
+            v = 0
+            actions = state.getLegalActions(g_idx)
+            p = 1 / len(actions)
+
+            for action in actions:
+                if g_idx == num_ghosts:
+                    v += p * max_value(state.generateSuccessor(g_idx, action), depth+1)[0]
+                else:
+                    v += p * chance(state.generateSuccessor(g_idx, action), depth, g_idx+1)
+
+            #print("mean value: ", v)
+            return v          
+
+        def max_value(state, depth):
+            best_action = None
+            # if trigger score evaluate funtion
+            if state.isWin() or state.isLose() or depth == self.depth:
+                return self.evaluationFunction(state), best_action
+            v = -99999999999999
+            for action in state.getLegalActions(0):
+                v_new = chance(state.generateSuccessor(0, action), depth, 1)
+                #return the best action
+                if v < v_new:
+                    v = v_new
+                    best_action = action
+            #print("max value: ", v)
+            return v, best_action
+
+        #number of ghosts
+        num_ghosts = gameState.getNumAgents()-1
+        #get the action with the max score
+        _, best_action = max_value(gameState, 0)
+
+        return best_action
+        #util.raiseNotDefined()
 
 def betterEvaluationFunction(currentGameState):
     """
@@ -248,9 +330,57 @@ def betterEvaluationFunction(currentGameState):
     evaluation function (question 5).
 
     DESCRIPTION: <write something here so we know what you did>
+    get the heuristic distance of nearest food and ghost
+    if in scare time, score of distance to ghost = -20, 
+    I guess that ghost = -20 is best because it is the half of capsule moves(40)  
+
     """
     "*** YOUR CODE HERE ***"
-    util.raiseNotDefined()
+    
+    # Useful information you can extract from a GameState (pacman.py)
+    current_pos = currentGameState.getPacmanPosition()
+    current_food = currentGameState.getFood()
+    current_food_list = current_food.asList()
+    current_ghost_states = currentGameState.getGhostStates()
+    current_ghost_pos = currentGameState.getGhostPositions()
+    num_ghosts = currentGameState.getNumAgents()-1
+    current_scared_times = [ghostState.scaredTimer for ghostState in current_ghost_states]
+    
+    #heuristic distance to food 
+    dis_food = [((current_pos[0]-f[0])**2 + (current_pos[1]-f[1])**2)**0.5 for f in current_food_list]
+    #if food is not empty
+    if dis_food:
+        food_nearest_idx = dis_food.index(min(dis_food))
+        pos_food_nearest = current_food_list[food_nearest_idx]
+        dis_food_nearest = dis_food[food_nearest_idx]
+    else: 
+        dis_food_nearest = 1
+    #heuristic distance to ghost
+    dis_ghost = [((current_pos[0]-g[0])**2 + (current_pos[1]-g[1])**2)**0.5 for g in current_ghost_pos]
+    ghost_nearest_idx = dis_ghost.index(min(dis_ghost))
+    pos_ghost_nearest = current_ghost_pos[ghost_nearest_idx]
+    dis_ghost_nearest = dis_ghost[ghost_nearest_idx]
+
+
+    for i in range(len(current_scared_times)):
+        if current_scared_times[i] != 0:
+            #print("scare time at ghost: {}".format(i+1))
+            #print("time: {}".format(current_scared_times))
+            #if in scare time, score of distance to ghost = -20 (half of capsule moves)
+            score_dis_ghost = 20
+        else:
+            # distance to ghost: ther further, the better
+            score_dis_ghost = - 1 / dis_ghost_nearest if dis_ghost_nearest != 0 else 0
+            
+    # distance to nearest food: ther closer, the better
+    score_dis_food = 1 / dis_food_nearest if dis_food_nearest != 0 else 0
+    
+    # pacman overall score 
+    score = currentGameState.getScore() + score_dis_food + score_dis_ghost
+
+    return score
+    
+    #util.raiseNotDefined()
 
 # Abbreviation
 better = betterEvaluationFunction
